5aaa9570223ed08f862c6ac669e8788a710a7768
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-02-25 12:19:03 +0100

    create reusable SectionHeading component and applied in about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 import { aboutList } from "@/utils/aboutLinks";
+import SectionHeading from "./SectionHeading";
 
 function AboutSection() {
   return (
     <section id="about" className="w-full  py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg">Introduction</h4>
-      <h2 className="h2 text-center text-5xl">About me</h2>
+      <SectionHeading
+        title="About Me"
+        description="Passionate about building user-friendly, responsive, and scalable
+            web applications. Skilled in using TailwindCSS, TypeScript, Next.js,
+            and consuming and implementing REST APIs to create efficient and
+            aesthetically pleasing UI/UX designs."
+      />
       <div className="flex w-full flex-col lg:flex-row items-center gap-10 my-20">
         <div className="w-[330px] md:w-[440px] lg:w-[550px] max-w-none">
           <Image src={assets.ComputerImg1} alt="computer" className="w-full" />
         </div>
         <div className="flex-1">
-          <p className="mb-10   lg:text-left">
-            Passionate about building user-friendly, responsive, and scalable
-            web applications. Skilled in using TailwindCSS, TypeScript, Next.js,
-            and consuming and implementing REST APIs to create efficient and
-            aesthetically pleasing UI/UX designs.
-          </p>
-
           <ul className="grid grid-cols-3 lg:grid-cols-3 gap-6 max-w-2xl">
             {aboutList.map((list, index) => {
               const { icon, title, description } = list;
