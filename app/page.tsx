7793e8a4f10d184818c59de6dcb85260ee21633d
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-03-02 14:32:27 +0100

    add project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
+import ProjectSection from "@/components/ProjectSection";
 
 function HomePage() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
     </main>
   );
 }
---
+++ components/ProjectSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionHeading from "./SectionHeading";
+
+function ProjectSection() {
+  return (
+    <section id="project">
+      <SectionHeading
+        title="Projects"
+        description="Few projects that I made using React.Js"
+      />
+    </section>
+  );
+}
+
+export default ProjectSection;
