fd802ecd281fc0d66c6e6995572e565e3316c9c7
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-03-10 12:23:29 +0100

    create basic form container

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -3,12 +3,33 @@ import SectionHeading from "./SectionHeading";
 
 function ContactSection() {
   return (
-    <section id="contact">
+    <section id="contact" className="py-10 scroll-mt-20">
       <SectionHeading
         title="Contact"
         description="
-      Do not hesitate to reach out, to get in touch with me"
+      Do not hesitate to reach out, to get in touch with me."
       />
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-[30px]">
+          <div className="lg:h-[54%] order-2 lg:order-none">
+            <form
+              action=""
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
+              <h3 className="h3 text-green-600 ">Let´s work together</h3>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <input type="firstname" placeholder="Firstname" />
+                <input type="lastname" placeholder="Lastname" />
+                <input type="email" placeholder="Email address" />
+                <input type="phone" placeholder="Phone number" />
+              </div>
+            </form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
