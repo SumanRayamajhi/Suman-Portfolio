cf124b4f3f54b19efe8521a0468419918fe1c24c
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-03-10 12:03:42 +0100

    add contact section with title heading and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
 import HeroSection from "@/components/HeroSection";
 import ProjectSection from "@/components/ProjectSection";
 
@@ -8,6 +9,7 @@ function HomePage() {
       <HeroSection />
       <AboutSection />
       <ProjectSection />
+      <ContactSection />
     </main>
   );
 }
---
+++ components/ContactSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SectionHeading from "./SectionHeading";
+
+function ContactSection() {
+  return (
+    <section id="contact">
+      <SectionHeading
+        title="Contact"
+        description="
+      Do not hesitate to reach out, to get in touch with me"
+      />
+    </section>
+  );
+}
+
+export default ContactSection;
