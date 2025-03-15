e2cd29a2b01a03773c56a73900e6910072389044
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-03-15 13:48:00 +0100

    remove unwanted code from header section

--- components/HeaderSection.tsx
+++ components/HeaderSection.tsx
@@ -69,48 +69,6 @@ function HeaderSection() {
         </div>
       </nav>
       <MobileMenu />
-      {/*  <div className="flex items-center space-x-6 lg:hidden>">
-        <button
-          onClick={toggleMobileMenu}
-          className=" flex flex-col justify-center items-end w-8 h-8 focus:outline-none group gap-1 lg:hidden"
-          aria-label="Toggle menu"
-        >
-          <span
-            className={`block w-4 h-0.5 bg-black transform transition-all duration-300 rounded-sm  ${
-              isMobileMenuOpen ? "rotate-45 w-[23px] translate-y-[6px] " : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-6 h-0.5 bg-black transform transition-all duration-300 rounded-sm ${
-              isMobileMenuOpen ? "opacity-0 scale-x-0" : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-5 h-0.5 bg-black transform transition-all duration-300 rounded-sm ${
-              isMobileMenuOpen ? "-rotate-45 w-6 -translate-y-1.5 bg-black" : ""
-            }`}
-          ></span>
-        </button>
-      </div>
-
-      <div
-        className={`fixed top-[10vh] left-0 w-full h-screen lg:hidden bg-slate-200 flex flex-col items-center justify-center transition-transform duration-300 ${
-          isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        <section className="flex flex-col font-semibold space-y-6">
-          {navItems.map((item, index) => (
-            <Link
-              href={item.href}
-              key={index}
-              className="uppercase font-medium hover:text-emerald-600 hover:transition-all hover:border-b-2 "
-              onClick={toggleMobileMenu}
-            >
-              {item.label}
-            </Link>
-          ))}
-        </section>
-      </div> */}
     </header>
   );
 }
