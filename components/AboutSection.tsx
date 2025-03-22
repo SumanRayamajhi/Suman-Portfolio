5d07c2366c1442373bea7a373df6d302dd0979da
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-03-22 14:42:22 +0100

    make better looking design of about section, and complete skill part

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -5,48 +5,84 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { RiUserSettingsFill } from "react-icons/ri";
 import { FaGraduationCap } from "react-icons/fa6";
 import { GoProjectSymlink } from "react-icons/go";
+import { skillsList } from "@/utils/aboutLinks";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 function AboutSection() {
   return (
     <section
       id="about"
-      className="w-full scroll-mt-20 py-16 px-4 sm:px-8 lg:px-16 bg-gray-200"
+      className="flex flex-col items-center w-full scroll-mt-14 py-16 px-4 sm:px-8 lg:px-16 bg-gray-200"
     >
       <SectionHeading
         title="About Me"
         description="Passionate about building user-friendly, responsive, and scalable
-            web applications. Skilled in using TailwindCSS, TypeScript, Next.js,
-            and consuming and implementing REST APIs to create efficient and
-            aesthetically pleasing UI/UX designs."
+            web applications. Committed to creating seamless experiences by leveraging the latest technologies and best practices in web development."
       />
-      <div className="flex w-full flex-col lg:flex-row items-center gap-10 my-20">
-        <div className="w-[330px] md:w-[440px] lg:w-[550px] max-w-none">
-          <Image src={assets.ComputerImg1} alt="computer" className="w-full" />
+      <div className="flex flex-col lg:flex-row items-center justify-between w-full gap-12 mt-16 lg:items-stretch">
+        <div className="w-full max-w-sm lg:max-w-md xl:max-w-lg flex-shrink-0 ">
+          <Image
+            src={assets.ComputerImg1}
+            alt="computer"
+            className="w-full"
+            priority
+          />
         </div>
-        <div className="flex-1">
-          <Tabs defaultValue="Skills">
-            <TabsList
-              className="grid w-full grid-cols-3 gap-7
-            "
-            >
-              <TabsTrigger value="Skills" className="gap-3">
-                <span>
-                  <RiUserSettingsFill size={25} />
-                </span>
+        <div className="flex-1 w-full flex flex-col justify-center">
+          <Tabs defaultValue="Skills" className="w-full">
+            <TabsList className="flex flex-col md:flex-row justify-center gap-4 py-20 md:py-0">
+              <TabsTrigger
+                value="Skills"
+                className="flex items-center gap-2 px-6 py-2 text-lg rounded-md"
+              >
+                <RiUserSettingsFill size={25} />
                 Skills
               </TabsTrigger>
-              <TabsTrigger value="Education" className="gap-3">
-                <span>
-                  <FaGraduationCap size={25} />
-                </span>
+              <TabsTrigger
+                value="Education"
+                className="flex items-center gap-2 px-6 py-2 text-lg rounded-md"
+              >
+                <FaGraduationCap size={25} />
                 Education
               </TabsTrigger>
-              <TabsTrigger value="Projects" className="gap-3">
+              <TabsTrigger
+                value="Projects"
+                className="flex items-center gap-2 px-6 py-2 text-lg rounded-md"
+              >
                 <GoProjectSymlink size={25} />
                 Projects
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="Skills">Skills</TabsContent>
+            <TabsContent value="Skills" className="w-full py-6">
+              <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 place-items-center">
+                {skillsList.map((skill, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className="hover:scale-105 transition-transform"
+                    >
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-20 h-20 sm:w-24 sm:h-24  bg-stone-400 rounded-lg flex justify-center items-center shadow-md transition group">
+                            <div className="text-5xl text-gray-100 group-hover:text-green-600">
+                              {skill.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.title}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  );
+                })}
+              </ul>
+            </TabsContent>
             <TabsContent value="Education">Education</TabsContent>
             <TabsContent value="Projects">Projects</TabsContent>
           </Tabs>
