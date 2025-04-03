0ddcc65d0b5535fccbdd7c8310dc67602c22f6b9
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-04-04 00:43:33 +0200

    correct DOM property from logo svg

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -25,11 +25,11 @@ function Logo() {
         <text
           x="50%"
           y="49%"
-          font-family="Arial, sans-serif"
-          font-size="27"
-          font-weight="bold"
+          fontFamily="Arial, sans-serif"
+          fontSize="27"
+          fontWeight="bold"
           fill="#4A4D4E"
-          text-anchor="middle"
+          textAnchor="middle"
           letterSpacing="-0.6"
           className="group-hover:fill-green-600 transition-colors duration-300"
         >
@@ -38,11 +38,11 @@ function Logo() {
         <text
           x="74%"
           y="58%"
-          font-family="Arial, sans-serif"
-          font-size="35"
-          font-weight="bold"
+          fontFamily="Arial, sans-serif"
+          fontSize="35"
+          fontWeight="bold"
           fill="#4A4D4E"
-          text-anchor="middle"
+          textAnchor="middle"
           className="group-hover:fill-green-600 transition-colors duration-300"
         >
           O
@@ -59,74 +59,6 @@ function Logo() {
         <text
           x="50%"
           y="74%"
-          font-family="Arial, sans-serif"
-          font-size="10"
-          font-weight="bold"
-          fill="#4A4D4E"
-          letter-spacing="2"
-          text-anchor="middle"
-          className="group-hover:fill-green-500 transition-colors duration-300"
-        >
-          SUMAN RAYAMAJHI
-        </text>
-      </svg>
-    </Link>
-    /*  <Link href="/" className="inline-block group">
-      <svg
-        width="auto"
-        viewBox="0 -10 300 100"
-        xmlns="http://www.w3.org/2000/svg"
-        className="cursor-pointer h-auto"
-      >
-        <text
-          x="27%"
-          y="50%"
-          fontFamily="Arial, sans-serif"
-          fontSize="35"
-          fontWeight="bold"
-          fill="#4A4D4E"
-          textAnchor="middle"
-          className="group-hover:fill-green-600 transition-colors duration-300"
-        >
-          P
-        </text>
-        <text
-          x="50%"
-          y="46%"
-          fontFamily="Arial, sans-serif"
-          fontSize="27"
-          fontWeight="bold"
-          fill="#4A4D4E"
-          textAnchor="middle"
-          letterSpacing="-0.6"
-          className="group-hover:fill-green-600 transition-colors duration-300"
-        >
-          ORTFOLI
-        </text>
-        <text
-          x="74%"
-          y="50%"
-          fontFamily="Arial, sans-serif"
-          fontSize="35"
-          fontWeight="bold"
-          fill="#4A4D4E"
-          textAnchor="middle"
-          className="group-hover:fill-green-600 transition-colors duration-300"
-        >
-          O
-        </text>
-        <line
-          x1="38%"
-          y1="52%"
-          x2="62%"
-          y2="52%"
-          stroke="#4A4D4E"
-          strokeWidth="1.5"
-          className="group-hover:stroke-green-500 transition-colors duration-300"
-        />
-        <text
-          x="50%"
-          y="65%"
           fontFamily="Arial, sans-serif"
           fontSize="10"
           fontWeight="bold"
@@ -138,7 +70,7 @@ function Logo() {
           SUMAN RAYAMAJHI
         </text>
       </svg>
-    </Link> */
+    </Link>
   );
 }
 
