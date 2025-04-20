6a88476f0662c87de5d502bb2c4a77247ec02e24
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-04-21 00:23:58 +0200

    improve and fix bug in contact section

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -80,16 +80,30 @@ function ContactSection() {
             <ul className="flex flex-col gap-10">
               {contactList.map((contact, index) => {
                 const { icon, title, description } = contact;
+
+                const isLinkedin = index === contactList.length - 1;
                 return (
                   <li key={index} className="flex items-center gap-6">
                     <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-stone-300 text-green-600 rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{icon}</div>
+                      <div className="text-[28px]">
+                        {isLinkedin ? (
+                          <a href="https://www.linkedin.com/in/suman-rayamajhi">
+                            {icon}
+                          </a>
+                        ) : (
+                          icon
+                        )}
+                      </div>
                     </div>
                     <div className="flex.1">
                       <p>{title}</p>
-                      <a href="https://www.linkedin.com/in/suman-rayamajhi">
+                      {isLinkedin ? (
+                        <a href="https://www.linkedin.com/in/suman-rayamajhi">
+                          <h3 className="h3">{description}</h3>
+                        </a>
+                      ) : (
                         <h3 className="h3">{description}</h3>
-                      </a>
+                      )}
                     </div>
                   </li>
                 );
