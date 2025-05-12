7e3f68af06f47f3e52c8285f0fc12c74e0a68389
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-05-13 00:23:04 +0200

    improve the card of aboutSection's educations and experiences

--- components/About/AboutContent.tsx
+++ components/About/AboutContent.tsx
@@ -8,14 +8,14 @@ function AboutContent({
   description,
 }: AboutContentTypes) {
   return (
-    <div className="h-[200px] md:h-[184px] py-10 px-8 md:px-10 rounded-md flex flex-col justify-center items-center bg-stone-100 shadow-md transition group gap-2">
+    <div className="h-full py-4 lg:py-8 px-5 lg:px-8 rounded-md flex flex-col justify-center items-center bg-stone-100 shadow-md transition group gap-2">
       <div className="flex gap-2">
         <span className="text-green-600">{icon}</span>
         <h4 className="font-bold">{duration}</h4>
       </div>
 
-      <h3 className="h3 lg:text-[20px]">{title}</h3>
-      <p className="text-green-600">{organization}</p>
+      <h3 className="h3 lg:text-[20px] text-center">{title}</h3>
+      <p className="text-green-600 font-medium text-center">{organization}</p>
       <p className="text-center">{description}</p>
     </div>
   );
